eca55ca91da2ca9c62d7f13cc3b3cbd7a2673fd9
Raghav Ganesh <59568704+msraghavganesh@users.noreply.github.com>
2021-09-02 23:27:51 +0530

    Dynamic exercise list in dashboard

--- seed.sql
+++ seed.sql
@@ -3,5 +3,5 @@ INSERT INTO exercise (id, name, subtitle, description) VALUES(2, 'Vrikshasana', 
 INSERT INTO exercise (id, name, subtitle, description) VALUES(3, 'Adho Mukho Svanasan', 'Downward Facing Dog Pose', 'This pose stretches hamstrings, chest and lengthens the spine, providing additional blood flow to the head.');
 INSERT INTO exercise (id, name, subtitle, description) VALUES(4, 'Trikonasana', 'Triangle Pose', 'It stretches the legs and torso, mobilises the hips and promotes deep breathing, leaving one with enlivening effects');
 INSERT INTO exercise (id, name, subtitle, description) VALUES(5, 'Kursiasana', 'Chair Pose', 'An intensely powerful pose, this one strengthens the muscles of the legs and arms.');
-INSERT INTO exercise (id, name, subtitle, description) VALUES(6, 'Naukasana', 'Boat Pose', 'It tightens the abdominal muscles and strengthens shoulders and upper back.');
+INSERT INTO exercise (id, name, subtitle, description) VALUES(6, 'Naukasana', 'Boat Pose', 'It tightens the abdominal muscles and strengthens shoulders and upper back.'); 
 INSERT INTO exercise (id, name, subtitle, description) VALUES(7, 'Bhujangasana', 'Cobra Pose', 'This one will strengthen the lower back muscles while cushioning the spine, triceps and opens the chest to promote the inhalations.');
