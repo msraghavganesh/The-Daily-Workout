250d73459aac165c19eee00a8b603511c8863c2a
Raghav Ganesh <59568704+msraghavganesh@users.noreply.github.com>
2021-09-02 23:28:29 +0530

    Setup database

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '7lBy4UYclr2khl/ViOPO1WbFKTSllV/TCwnf3R+AjBg='
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
